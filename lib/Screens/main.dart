4e77c5e59dfca1976813a1c40f6676ce5801535d
Rewaa Adel <Rewaa1998a@outlook.com>
2021-02-19 21:56:46 +0300

    Commit

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -2,16 +2,18 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tester/Screens/Administrator/homepage_administrator.dart';
+import 'package:tester/Screens/SignIn.dart';
 import 'package:tester/Screens/signUp.dart';
 import 'package:tester/Screens/style.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'dart:io' show Platform;
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp app = await Firebase.initializeApp(
-    name: 'db2',
+  final FirebaseApp checkpoint = await Firebase.initializeApp(
+    name: 'checkpoint',
     options: Platform.isIOS || Platform.isMacOS
         ? FirebaseOptions(
             appId: '1:297855924061:ios:c6de2b69b03a5be8',
@@ -36,63 +38,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(alignment: Alignment.center),
-              Container(
-                margin: EdgeInsets.fromLTRB(70, 0, 70, 70),
-                child: Image.asset(
-                  'Assets/logowithname.png',
-                  height: 200,
-                ),
-              ),
-              Text_Field(label: "Email or Id", secure: false),
-              Text_Field(label: "Password", secure: true),
-              Container(
-                child: TextButton(
-                  child: Text("Forget my password"),
-                  onPressed: () {
-                    DatabaseReference def =
-                        FirebaseDatabase.instance.reference().child('Test');
-                    def.set('IsConnect');
-                  },
-                ),
-              ),
-              SubmitButtons(
-                text: "Sign In",
-                onpressed: () {
-                  runApp(homePageAdministrator());
-                },
-              ),
-              Container(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                    Container(
-                      child: Text(
-                        "Don\' have an account?",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ),
-                    Container(
-                      child: TextButton(
-                        child: Text("Sign up"),
-                        onPressed: () {
-                          runApp(signUp());
-                        },
-                      ),
-                    )
-                  ]))
-            ],
-          )),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: SignIn());
   }
 }
--- lib/Screens/signUp.dart
+++ lib/Screens/signUp.dart
@@ -1,14 +1,58 @@
 //import 'dart:html';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:tester/Screens/AcademicStaff/homePageAS.dart';
+import 'package:tester/Screens/Administrator/homepage_administrator.dart';
+import 'package:tester/Screens/SignIn.dart';
 import 'package:tester/Screens/main.dart';
 import 'package:tester/Screens/style.dart';
 
-class signUp extends StatelessWidget {
-  // This widget is the root of your application.
+class SignUp extends StatefulWidget {
+  SignUp({Key key}) : super(key: key);
+
   @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
   String position;
+  static const String id = 'SignUp';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _positionController = TextEditingController();
+
+  var Position = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _idController.dispose();
+    _positionController.dispose();
+
+    super.dispose();
+  }
+
+  void regester() async {
+    var user = FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text, password: _passwordController.text);
+
+    if (user != null) {
+      print(user);
+      //runApp(homePageAdministrator());
+    } else {
+      print('Try ag');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
@@ -19,71 +63,171 @@ class signUp extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.white,
             body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(70, 0, 70, 70),
-                  child: Image.asset(
-                    'Assets/logowithname.png',
-                    height: 200,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(70, 0, 70, 70),
+                    child: Image.asset(
+                      'Assets/logowithname.png',
+                      height: 200,
+                    ),
                   ),
+                  Container(
+                      height: 45,
+                      margin: EdgeInsets.symmetric(horizontal: 70, vertical: 5),
+                      child: TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          labelText: 'Name',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4))),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Fill Name Input';
+                          } else {
+                            return ' ';
+                          }
+                        },
+                      )),
+                  Container(
+                      height: 45,
+                      margin: EdgeInsets.symmetric(horizontal: 70, vertical: 9),
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          labelText: 'Email',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4))),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Fill Email Input';
+                          } else {
+                            return ' ';
+                          }
+                        },
+                      )),
+                  Container(
+                      height: 45,
+                      margin: EdgeInsets.symmetric(horizontal: 70, vertical: 5),
+                      child: TextFormField(
+                        controller: _idController,
+                        decoration: InputDecoration(
+                          hintText: 'Id Number',
+                          labelText: 'Id Number',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4))),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Fill Id Number Input';
+                          } else {
+                            return ' ';
+                          }
+                        },
+                      )),
+                  /* Text_Field(
+                  controller: _nameController,
+                  label: "Name",
+                  secure: false,
                 ),
-                Text_Field(label: "Name", secure: false),
-                Text_Field(label: "Email", secure: false),
-                Text_Field(label: "Id Number", secure: false),
-                Container(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-                  margin: EdgeInsets.only(bottom: 10),
-                  width: 300,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(4)),
-                  child: DropdownButton(
-                    isExpanded: true,
-                    hint: Text("Position"),
-                    value: position,
-                    onChanged: (value) {
-                      //setState(() {
-                      //position = value;
-                      //});
+                Text_Field(
+                    controller: _emailController,
+                    label: "Email",
+                    secure: false),
+                Text_Field(
+                    controller: _idController,
+                    label: "Id Number",
+                    secure: false), */
+                  Container(
+                    height: 45,
+                    margin: EdgeInsets.symmetric(horizontal: 70, vertical: 5),
+                    // padding: EdgeInsets.only(left: 10, right: 10),
+                    //margin: EdgeInsets.only(bottom: 10),
+                    //  width: 300,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(4)),
+                    child: DropdownButton(
+                      isExpanded: true,
+                      hint: Text("  Position"),
+                      value: position,
+                      onChanged: (value) {
+                        setState(() {
+                          _idController = value;
+                        });
+                      },
+                      items: ['Academic Staff', 'Student'].map((value) {
+                        return new DropdownMenuItem(
+                            value: value,
+                            child: Text(
+                              value,
+                            ));
+                      }).toList(),
+                    ),
+                  ),
+                  /* Text_Field(
+                    controller: _passwordController,
+                    label: "Password",
+                    secure: true), */
+                  Container(
+                      height: 45,
+                      margin: EdgeInsets.symmetric(horizontal: 70, vertical: 5),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          labelText: 'Password',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4))),
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please Fill Password Input';
+                          } else {
+                            return ' ';
+                          }
+                        },
+                      )),
+                  SubmitButtons(
+                    text: "Sign Up",
+                    onpressed: () async {
+                      regester();
                     },
-                    items: ['Academic Staff', 'Student'].map((value) {
-                      return new DropdownMenuItem(
-                          value: value,
-                          child: Text(
-                            value,
-                          ));
-                    }).toList(),
                   ),
-                ),
-                Text_Field(label: "Password", secure: true),
-                SubmitButtons(
-                  text: "Sign Up",
-                  onpressed: () {
-                    runApp(homepageAS());
-                  },
-                ),
-                Container(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                      Container(
-                        child: Text(
-                          "Already have an account?",
-                          style: TextStyle(color: Colors.grey),
+                  Container(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                        Container(
+                          child: Text(
+                            "Already have an account?",
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ),
-                      ),
-                      Container(
-                        child: TextButton(
-                          child: Text("Sign in"),
-                          onPressed: () {
-                            runApp(MyApp());
-                          },
-                        ),
-                      )
-                    ]))
-              ],
-            )));
+                        Container(
+                            child: TextButton(
+                                child: Text("Sign in"),
+                                onPressed: () async {
+                                  var result = await FirebaseAuth.instance
+                                      .createUserWithEmailAndPassword(
+                                          email: _emailController.text,
+                                          password: _passwordController.text);
+                                  if (result != null) {
+                                    runApp(SignIn());
+                                  } else {
+                                    print('please try later');
+                                  }
+                                }))
+                      ])),
+                ])));
   }
 }
